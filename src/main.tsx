2964778152f177b763b44c50b699f6fa02a0f955
Atef Ataya <atef.ataya@gmail.com>
2023-05-11 10:14:07 +0400

    Customizing Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, // 5 minutes
+//       staleTime: 10 * 1000, // 10 seconds
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// });
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
