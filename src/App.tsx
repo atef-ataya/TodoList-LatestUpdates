6d0b9d9049fe93e21e008c1984461d5c7cb37f69
atef.ataya <atef.ataya@gmail.com>
2023-05-18 09:15:40 +0400

    Creating auth reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
-import Counter from './state-management/Counter';
-import TaskList from './state-management/TaskList';
+import LoginStatus from './state-management/LoginStatus';
 
 function App() {
   return (
     <>
-     <TaskList/>
+      <LoginStatus />
     </>
   );
 }
---
+++ src/state-management/reducers/authReducer.ts
@@ -0,0 +1,23 @@
+interface login {
+  type: 'LOGIN';
+  user: string;
+}
+
+interface logout {
+  type: 'LOGOUT';
+}
+
+type userAction = login | logout;
+
+const authReducer = (user: string, action: userAction): string => {
+  switch (action.type) {
+    case 'LOGIN':
+      return (user = action.user);
+    case 'LOGOUT':
+      return (user = '');
+    default:
+      return user;
+  }
+};
+
+export default authReducer;
